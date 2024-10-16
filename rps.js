16148fe22b4af2170fefaffce0526e675e9d2a16
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-16 14:30:15 +0000

    Refactored the code inside the function to use less if and else statements

--- rps.js
+++ rps.js
@@ -1,28 +1,20 @@
-function rockPaperScissors(player1, player2) {
+const rockPaperScissors = (player1, player2) => {
   if (player1 === player2) {
     return "draw";
-  } else if (player1 === "paper" && player2 === "rock" ) {
-    return "player1";
-  } else if (player1 === "rock" && player2 === "scissors") {
-    return "player1";
-  } else if (player1 === "scissors" && player2 === "paper") {
-    return "player1";
-  } else if (player1 === "lizard" && player2 === "spock") {
-    return "player1";
-  } else if (player1 === "spock" && player2 == "scissors") {
-    return "player1";
-  } else if (player1 === "rock" && player2 === "lizard") {
-    return "player1";
-  } else if (player1 === "paper" && player2 === "spock") {
-    return "player1";
-  } else if (player1 === "scissors" && player2 === "lizard") {
-    return "player1";
-  } else if (player1 === "lizard" && player2 === "paper") {
-    return "player1";
-  } else if (player1 === "spock" && player2 === "rock") {
-    return "player1";
   } else {
-    return "player2";
+    if (player1 === "rock" && (player2 === "scissors" || player2 === "lizard")) {
+      return "player1";
+    } else if (player1 === "paper" && (player2 === "rock" || player2 === "spock")) {
+      return "player1";
+    } else if (player1 === "scissors" && (player2 === "paper" || player2 === "lizard")) {
+      return "player1";
+    } else if (player1 === "lizard" && (player2 === "spock" || player2 === "paper")) {
+      return "player1";
+    } else if (player1 === "spock" && (player2 === "scissors" || player2 === "rock")) {
+      return "player1"
+    } else {
+      return "player2";
+    }  
   }
 }
 
