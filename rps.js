a5f191acde09fd713e48c3b27ade8c1eba806218
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-16 13:27:24 +0000

    Added decisions for who should win in the rockPaperScissors function

--- rps.js
+++ rps.js
@@ -1,17 +1,30 @@
 function rockPaperScissors(player1, player2) {
-
-  
-   
+  if (player1 === player2) {
+    return "draw";
+  } else if (player1 === "paper" && player2 === "rock" ) {
+    return "player1";
+  } else if (player1 === "rock" && player2 === "scissors") {
+    return "player1";
+  } else if (player1 === "scissors" && player2 === "paper") {
+    return "player1";
+  } else if (player1 === "lizard" && player2 === "spock") {
+    return "player1";
+  } else if (player1 === "spock" && player2 == "scissors") {
+    return "player1";
+  } else if (player1 === "rock" && player2 === "lizard") {
+    return "player1";
+  } else if (player1 === "paper" && player2 === "spock") {
+    return "player1";
+  } else if (player1 === "scissors" && player2 === "lizard") {
+    return "player1";
+  } else if (player1 === "lizard" && player2 === "paper") {
+    return "player1";
+  } else if (player1 === "spock" && player2 === "rock") {
+    return "player1";
+  } else {
+    return "player2";
+  }
 }
-
-
-
-
-
-
-
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
